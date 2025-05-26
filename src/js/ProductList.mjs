a8aa97ac4fcd2a9ee2812fadae5302032ee4758f
Vad2099 <valdemarlasnibat@hotmail.es>
2025-05-26 00:03:33 -0600

    I need to fixk some issues

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -5,7 +5,7 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
     return `<li class="product-card">
       <a href="product_pages/?product= ${product.Id}">
-        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
         <h2 class="card__brand">${product.Name}</h2>
         <h3 class="card__name">${product.NameWithoutBrand}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
@@ -26,8 +26,8 @@ export default class ProductList {
     }
   
     async init() {
-      // the dataSource will return a Promise...so you can use await to resolve it.
-      const list = await this.dataSource.getData();
+      // the init method will be called to initialize the product list.
+      const list = await this.dataSource.getData(this.category);
         // next, render the list – ** future **
         this.renderList(list);
     }
