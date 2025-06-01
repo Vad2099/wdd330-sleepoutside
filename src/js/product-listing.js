f1daaeb4ce80f29ec358399469e59b3ebf22beb6
Vad2099 <valdemarlasnibat@hotmail.es>
2025-05-31 23:11:07 -0600

    week 4 team activity added

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,25 +1,12 @@
 import { loadHeaderFooter, getParam } from "./utils.mjs";
-// Import the ProductData class from the module
-import ProductData from "./ProductData.mjs";
-
-// Create a instance of ProductList class in the main.js file
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 
 loadHeaderFooter();
 
 const category = getParam("category");
-
-//console.log("Product ID:", productId);
-
-// first create an instance of the ProductData class.
-const dataSource = new ProductData();
-
-// Si quieres probar que funciona, puedes obtener los datos así (opcional para prueba)
-//dataSource.getData().then(data => console.log(data));
-
-// then get the element you want the product list to render in
+const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list");
-// then create an instance of the ProductList class and send it the correct information.
-const productList = new ProductList(category, dataSource, element);
-// Call the init method to fetch and render the product list
-productList.init();
+const myList = new ProductList(category, dataSource, element);
+
+myList.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -27,11 +27,10 @@ export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get(param);
-  return product?.trim();
+  return product
 }
 
-// function to render a list of items using a template
-export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) { 
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
   const htmlStrings = list.map(template);
   // if clear is true we need to clear out the contents of the parent.
   if (clear) {
@@ -40,27 +39,26 @@ export function renderListWithTemplate(template, parentElement, list, position =
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
 
-// function to render a header using a template
 export function renderWithTemplate(template, parentElement, data, callback) {
   parentElement.innerHTML = template;
-  if(callback) {
+  if (callback) {
     callback(data);
   }
 }
 
-// This asynchronous function fetches the content of the HTML file given a path. The response to the fetch is converted to text and returns the HTML content as a string.
-export async function loadTemplate(path) {
-  const response = await fetch(path);
-  const template = await response.text();
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
   return template;
 }
 
-// This function takes a template string and a data object, and replaces the placeholders in the template with the corresponding values from the data object. It returns the modified template string.
 export async function loadHeaderFooter() {
   const headerTemplate = await loadTemplate("../partials/header.html");
   const footerTemplate = await loadTemplate("../partials/footer.html");
+
   const headerElement = document.querySelector("#main-header");
   const footerElement = document.querySelector("#main-footer");
-  renderWithTemplate(headerTemplate, headerElement);  
+
+  renderWithTemplate(headerTemplate, headerElement);
   renderWithTemplate(footerTemplate, footerElement);
 }
